ad5c9dc908df2b7b06787e09dbd95cc0be5a4933
James <jimcummins@protonmail.com>
2024-08-18 19:50:50 -0400

    Added basic files

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('auth-button').addEventListener('click', authenticateWithSpotify);
+document.getElementById('fetch-button').addEventListener('click', fetchTopArtists);
+
+function authenticateWithSpotify() {
+    // Spotify authentication logic here
+}
+
+function fetchTopArtists() {
+    // Fetch top artists logic here
+}
