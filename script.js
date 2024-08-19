d934f71aee29325bcba078d3bffb65b97c9472f5
James <jimcummins@protonmail.com>
2024-08-19 00:25:52 -0400

    Fixed loop

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ async function fetchTopArtists() {
         }
     }
     
-    responses = []
+    response_vals = []
     
     let body1 = await fetch('https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=50&offset=0', {headers: {Authorization: 'Bearer ' + token}});
     let response1 =await body1.json();
