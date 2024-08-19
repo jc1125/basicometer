62dde7456284bac2bcb83e9281e3b2ae698239e4
James <jimcummins@protonmail.com>
2024-08-18 20:56:39 -0400

    Ugh

--- script.js
+++ script.js
@@ -52,32 +52,27 @@ async function fetchTopArtists() {
     const urlParams = new URLSearchParams(window.location.search);
     let code = urlParams.get('code');
 
-    const getToken = async code => {
-
         // stored in the previous step
-        let codeVerifier = localStorage.getItem('code_verifier');
-      
-        const payload = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          body: new URLSearchParams({
-            client_id: clientId,
-            grant_type: 'authorization_code',
-            code,
-            redirect_uri: redirectUri,
-            code_verifier: codeVerifier,
-          }),
-        }
-      
-        const body = await fetch(url, payload);
-        const response =await body.json();
-      
-        localStorage.setItem('access_token', response.access_token);
+    let codeVerifier = localStorage.getItem('code_verifier');
+    
+    const payload = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
+        }),
     }
-
-    await getToken(code)
+    
+    const body = await fetch(url, payload);
+    const response =await body.json();
+    
+    localStorage.setItem('access_token', response.access_token);
 
     console.log("Token:")
     console.log(localStorage.getItem('access_token'))
